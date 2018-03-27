73abf1505d7ae5b08be7303dda1da48736217b9c
Obstinated <alon_msn@hotmail.com>
2018-03-26 23:32:20 -0600

    Actualizacion de BD

--- Clinica Server/AccesoDatos/ClinicaDataModel.Designer.cs
+++ Clinica Server/AccesoDatos/ClinicaDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Proyectos de GitHub\Clinica\Clinica\Clinica\AccesoDatos\ClinicaDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Proyectos de GitHub\Clinica\Clinica\Clinica Server\AccesoDatos\ClinicaDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
